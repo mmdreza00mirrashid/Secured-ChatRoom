99f34df64e39ab53c5814ee10dbb81c899c258d0
mmdreza00mirrashid <76827212+mmdreza00mirrashid@users.noreply.github.com>
2024-06-07 18:23:50 +0330

    Code now is compatible with Py3 and runs on local

--- chat_server.py
+++ chat_server.py
@@ -1,68 +1,130 @@
+# Python program to implement server side of chat room.
 import socket
 import select
-from threading import *
 import sys
 
+'''Replace "thread" with "_thread" for python 3'''
+from _thread import *
 
+"""The first argument AF_INET is the address domain of the 
+socket. This is used when we have an Internet Domain with 
+any two hosts The second argument is the type of socket. 
+SOCK_STREAM means that data or characters are read in 
+a continuous flow."""
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-"""
-the first argument AF_INET is the address domain of the socket. This is used when we have an Internet Domain
-with any two hosts
-The second argument is the type of socket. SOCK_STREAM means that data or characters are read in a continuous flow
-"""
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-if len(sys.argv) != 3:
-    print("Correct usage: script, IP address, port number")
-    exit()
-IP_address = str(sys.argv[1])
-Port = int(sys.argv[2])
-server.bind((IP_address, Port)) 
-#binds the server to an entered IP address and at the specified port number. The client must be aware of these parameters
+
+# checks whether sufficient arguments have been provided
+# if len(sys.argv) != 3:
+# 	print ("Correct usage: script, IP address, port number")
+# 	exit()
+
+# takes the first argument from command prompt as IP address
+IP_address = str('127.0.0.1')
+
+# takes second argument from command prompt as port number
+Port = 8080
+
+""" 
+binds the server to an entered IP address and at the 
+specified port number. 
+The client must be aware of these parameters 
+"""
+server.bind((IP_address, Port))
+
+""" 
+listens for 100 active connections. This number can be 
+increased as per convenience. 
+"""
 server.listen(100)
-#listens for 100 active connections. This number can be increased as per convenience
-list_of_clients=[]
+
+list_of_clients = []
+
 
 def clientthread(conn, addr):
-    conn.send("Welcome to this chatroom!")
-    #sends a message to the client whose user object is conn
+    # sends a message to the client whose user object is conn
+    isUsers = False
+    if len(list_of_clients) > 1:
+        conn.send("Welcome to this chatroom!".encode())
+        isUsers = True
+    else:
+        conn.send("Waiting for more users to join".encode())
+    conn.send(str(isUsers).encode())
+    conn.setblocking(False)
     while True:
-            try:     
-                message = conn.recv(2048)    
-                if message:
-                    print("<" + addr[0] + "> " + message)
-                    message_to_send = "<" + addr[0] + "> " + message
-                    broadcast(message_to_send,conn)
-                    #prints the message and address of the user who just sent the message on the server terminal
-                else:
-                    remove(conn)
-            except:
-                continue
+        try:
+            if len(list_of_clients) > 1 and isUsers == False:
+                isUsers = True
+                conn.send(str(isUsers).encode())
 
-def broadcast(message,connection):
+            message = conn.recv(1024).decode()
+            if message:
+
+                """prints the message and address of the 
+					user who just sent the message on the server 
+					terminal"""
+                print("<" + addr[0] + "> " + message)
+
+                # Calls broadcast function to send message to all
+                # message_to_send = "<" + addr[0] + "> " + message
+                message_to_send =message
+                broadcast(message_to_send, conn)
+
+            else:
+                remove(conn)
+                broadcast('user disconnected',conn)
+                return
+
+        except:
+            continue
+
+
+"""Using the below function, we broadcast the message to all 
+clients who's object is not the same as the one sending 
+the message """
+
+
+def broadcast(message, connection):
     for clients in list_of_clients:
-        if clients!=connection:
+        if clients != connection:
             try:
-                clients.send(message)
+                clients.send(message.encode())
             except:
                 clients.close()
+                print("disconnected")
+
+                # if the link is broken, we remove the client
                 remove(clients)
+
+
+"""The following function simply removes the object 
+from the list that was created at the beginning of 
+the program"""
+
 
 def remove(connection):
     if connection in list_of_clients:
         list_of_clients.remove(connection)
 
+
 while True:
+    """Accepts a connection request and stores two parameters, 
+	conn which is a socket object for that user, and addr 
+	which contains the IP address of the client that just 
+	connected"""
     conn, addr = server.accept()
-    """
-    Accepts a connection request and stores two parameters, conn which is a socket object for that user, and addr which contains
-    the IP address of the client that just connected
-    """
+
+    """Maintains a list of clients for ease of broadcasting 
+	a message to all available people in the chatroom"""
     list_of_clients.append(conn)
+
+    # prints the address of the user that just connected
     print(addr[0] + " connected")
-    #maintains a list of clients for ease of broadcasting a message to all available people in the chatroom
-    #Prints the address of the person who just connected
-    start_new_thread(clientthread,(conn,addr))
-    #creates and individual thread for every user that connects
+    print(type(conn))
+
+    # creates and individual thread for every user
+    # that connects
+    start_new_thread(clientthread, (conn, addr))
 
 conn.close()
 server.close()
--- client.py
+++ client.py
@@ -1,26 +1,52 @@
+# Python program to implement client side of chat room.
 import socket
 import select
 import sys
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-if len(sys.argv) != 3:
-    print "Correct usage: script, IP address, port number"
-    exit()
-IP_address = str(sys.argv[1])
-Port = int(sys.argv[2])
-server.connect((IP_address, Port))
+if __name__ == '__main__':
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # if len(sys.argv) != 3:
+    # 	print ("Correct usage: script, IP address, port number")
+    # 	exit()
+    UserName = input("Your username: ")
+    IP_address = str('127.0.0.1')
+    Port = 8080
+    server.connect((IP_address, Port))
+    isRunning=True
 
-while True:
-    sockets_list = [sys.stdin, server]
-    read_sockets,write_socket, error_socket = select.select(sockets_list, [], [])
-    for socks in read_sockets:
-        if socks == server:
-            message = socks.recv(2048)
-            print message
-        else:
-            message = sys.stdin.readline()
-            server.send(message)
-            sys.stdout.write("<You>")
-            sys.stdout.write(message)
-            sys.stdout.flush()
-server.close()
+    message = server.recv(1024).decode()
+    print("1",message)
+    isUsers = server.recv(1024).decode()
+    if isUsers=="True":
+        server.setblocking(False)
+
+    while isRunning:
+        try:
+            sockets_list = [sys.stdin, server]
+            read_sockets, write_socket, error_socket = select.select(sockets_list, [], [])
+            for socks in read_sockets:
+                if socks == server:
+                    if isUsers == "False":
+                        message = server.recv(1024).decode()
+                        if message:
+                            isUsers = "True"
+                            server.setblocking(False)
+                            print("There are new users to chat with!")
+                    elif isUsers == "True":
+                        print("its true")
+                        message = server.recv(1024).decode()
+                        print(message)
+                else:
+                    if isUsers == "True":
+                        message = sys.stdin.readline()
+                        if (message.strip(' \n\t')).lower() == "exit":
+                            isRunning=False
+                        else:
+                            server.send((f'<{UserName}> : {message}').encode())
+                            sys.stdout.write("<You>")
+                            sys.stdout.write(message)
+                            sys.stdout.flush()
+        except:
+            continue
+
+    server.close()
